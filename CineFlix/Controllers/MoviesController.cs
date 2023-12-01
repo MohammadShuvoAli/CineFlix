4b6e33d8a303197af267848b15956dfe815f5c56
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2023-12-01 19:13:33 +0600

    update

---
+++ CineFlix/Controllers/MoviesController.cs
@@ -0,0 +1,16 @@
+﻿using CineFlix.Models;
+using System.Web.Mvc;
+
+namespace CineFlix.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            return View(movie);
+        }
+    }
+}
---
+++ CineFlix/Models/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace CineFlix.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
